f91e47dfb118fbae2c7f63da65afddfe1bd2d25b
dimasprastyoaditama <prastyod647@gmail.com>
2025-03-25 09:59:10 +0700

    void inputan

--- file1.cpp
+++ file1.cpp
@@ -4,4 +4,15 @@ using namespace std;
 int arr[20]; // Membuat Array dengan panjang data 20
 int n; // Membuat Variable inputan n
 
-void input()
+void inputan()
+{ // Procedure Input
+while (true)
+{
+cout << "Masukkan Jumlah Data pada Array : "; // Membuat Inputan jumlahh element Array
+cin >> n; // memanggil variable inputan n
+
+if (n <= 20)
+{ // Membuat Kondisi n tidak lebih dari 20
+break;
+}
+else
